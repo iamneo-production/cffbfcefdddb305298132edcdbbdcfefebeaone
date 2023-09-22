3069840985cc7f11cf39544020debf6756734823
Iamneo <techadmin@iamneo.ai>
2023-09-22 08:48:16 +0000

    Backup Commit

--- second.tf
+++ second.tf
@@ -1,7 +1,16 @@
 terraform{
     required_providers{
         aws={
-            source
+            source="hashicorp/aws"
+            version ="~>4.0"
         }
     }
+}
+provider "aws"{
+    region="ap-northeast-1"
+    access_key="AKIA4WB7BXR6NJSMF3HZ"
+    secret_key="SFkyUtzQPtNbdYAnz7t9pglR9brWGIhr4b++MQmS"
+}
+resource "aws_instance" "example"{
+    ami="ami-0ed99df77a82560e6 "
 }
